f9a253679f3cccf4271c075b7ede8b17e9efd727
Mike Sherwood <coolhand2@gmail.com>
2020-05-28 20:46:39 -0600

    Changing AbstractEntity.clone to AbstractEntity.copy, to avoid accidentally calling Object.clone on an entity.

--- src/main/java/org/example/commons/AbstractEntity.java
+++ src/main/java/org/example/commons/AbstractEntity.java
@@ -5,10 +5,12 @@ abstract public class AbstractEntity<T> implements Comparable<T> {
     /**
      * The lombok-generated equals method override either goes too far, or not far enough (can't figure out which) and
      * mistakenly says that objects "this" and "that" are not the same, despite "that" being a clone of "this".
+     *
+     * So, we're forcing an override of the equals function, so we can use the commons-lang3
+     * EqualsBuilder#reflectionEquals method, which does pass this particular test.
      */
 
-
-    abstract public T clone();
+    abstract public T copy();
     abstract public int compareTo(T that);
     abstract protected boolean isEqualTo(T that);
     abstract protected int getHashCode();
